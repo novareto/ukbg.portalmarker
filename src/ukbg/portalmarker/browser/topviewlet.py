98d88f82d0f9ac70f42000b308e838e44db77342
trabby <info@trabucchi.de>
2018-07-25 13:38:57 +0200

    import user name on intranet nav

--- src/ukbg/portalmarker/browser/topviewlet.py
+++ src/ukbg/portalmarker/browser/topviewlet.py
@@ -1,6 +1,7 @@
 from zope.interface import Interface
 from plone.app.layout.viewlets.interfaces import IPortalTop
 from plone.app.layout.viewlets.interfaces import IPortalHeader
+from plone import api as ploneapi
 from uvc.api import api
 
 api.templatedir('templates')
@@ -11,3 +12,19 @@ class PortalMarkerViewlet(api.Viewlet):
 
     def update(self):
         self.greetings = 'Hallo Welt'
+	self.memberfullname = self.member_name_or_id()
+
+    def member_name_or_id(self, ):
+        """
+        """
+        if ploneapi.user.is_anonymous():
+            return ''
+
+        member = ploneapi.user.get_current()
+        fullname = member.getProperty('fullname', '')
+
+        if fullname:
+            return fullname
+        else:
+            return member.getId()
+
